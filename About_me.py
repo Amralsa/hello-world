c4e2efd2dc8119db75190efcbb3a59b53ddc8f8b
AmrAlsa <amr.alsabahi99@gmail.com>
2020-02-25 08:14:13 -0500

    added about_me

---
+++ About_me.py
@@ -0,0 +1,4 @@
+x = str(input('Are you a vistor? If so, please type "vistor": '))
+
+if x == "vistor":
+    print("Hello, there!\n My name is Amr Alsabahi, currently looking to make a great impact in the world. Sometimes, life can be hard, but we've managed to get this far.I don't think there is an option of going back! ")
